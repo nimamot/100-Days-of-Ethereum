bef2ae6eca8371bc5eb7e23f8a3c854174f616c6
nimamot <64922998+nimamot@users.noreply.github.com>
2023-07-03 22:47:34 -0700

    working on nav

--- Web3.0/client/src/components/Navbar.jsx
+++ Web3.0/client/src/components/Navbar.jsx
@@ -4,12 +4,21 @@ import { AiOutlineClose } from "react-icons/ai";
 import logo from "../../images/logo.png";
 import bitomoji from "../../images/Bitomoji_70 copy.png";
 
+const NavbarItem = ({ title, classprops }) => {
+  return <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>;
+};
+
 const Navbar = () => {
   return (
     <nav className="w-full flex md:justify-center">
       <div className="md:flex-[0.5] flex-initial items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
+      <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
+        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+          <NavbarItem key={item + index} title={item} />
+        ))}
+      </ul>
     </nav>
   );
 };
